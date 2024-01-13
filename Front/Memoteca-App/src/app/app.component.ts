ed3114fd20cd6ef3f491ba39205e7ab41b5529a8
Gabriel <guardavege@gmail.com>
2024-01-13 15:49:48 -0300

    Tow-way Data Binding Crated

--- Front/Memoteca-App/src/app/app.component.ts
+++ Front/Memoteca-App/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ThoughtsCreatedComponent } from './components/thoughts/thoughts-created/thoughts-created.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- Front/Memoteca-App/src/app/components/thoughts/thoughts-created/thoughts-created.component.ts
+++ Front/Memoteca-App/src/app/components/thoughts/thoughts-created/thoughts-created.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-thoughts-created',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './thoughts-created.component.html',
   styleUrl: './thoughts-created.component.css'
 })
+
 export class ThoughtsCreatedComponent {
+
+  thoughts = {
+    id : '1',
+    content : 'Learning Angular',
+    author : 'dev',
+    modelo : 'modelo 1'
+
+  }
+
+  createdThoughts()
+  {
+    alert("Thougths Created");
+  }
+
+  CancelSend()
+  {
+    alert("Send Cancel");
+  }
 
 }
