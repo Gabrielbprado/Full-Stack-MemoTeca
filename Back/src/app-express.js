0184c4cec74c67b0217d0c90d3f04384aae86bdf
Gabriel <guardavege@gmail.com>
2024-01-14 16:42:08 -0300

    Cors Adicionado

--- Back/src/app-express.js
+++ Back/src/app-express.js
@@ -1,10 +1,19 @@
-const Express = require('express');
-const route = require('./Routes/Index.js');
-const app = Express();
-route(app);
+const express = require('express');
+const cors = require('cors');
+const routes = require('./Routes/Index.js');
 
-app.get('/',(req,res) =>
-{
+const app = express();
+
+// Configurar CORS antes de aplicar as rotas
+app.use(cors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,json',
+}));
+
+// Aplicar as rotas
+routes(app);
+
+app.get('/', (req, res) => {
     res.status(200).send('Server Criado');
 });
 
